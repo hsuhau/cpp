ae818c83f27fb21a0df7d0dc5811885fd8f4a262
hsuhau <hsuhau@foxmail.com>
2021-03-26 14:05:03 +0800

    C++ Advanced Part III

--- 3-advanced/8_cpp_multithreading.cpp
+++ 3-advanced/8_cpp_multithreading.cpp
@@ -17,9 +17,9 @@ This tutorial assumes that you are working on Linux OS and we are going to write
 
 using namespace std;
 
-#define NUM_THREADS = 5
+#define NUM_THREADS 5
 
-void *PrintWord(void *threadid){
+void *PrintHello(void *threadid){
     long tid;
     tid = (long)threadid;
     cout << "Hello World! Thread ID, " << tid << endl;
@@ -27,6 +27,25 @@ void *PrintWord(void *threadid){
 }
 
 
+int main()
+{
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    int i;
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        cout << "main() : creating thread, " << i << endl;
+        rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+
+        if (rc)
+        {
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+        }
+    }
+    pthread_exit(NULL);
+}
 
 
 
